c7697a63a2e13eab5a8a8ddc949c2a7574093733
Jonathan Hunsucker <jonathantrevorh@gmail.com>
2020-05-05 18:44:49 -0400

    port to react, add lo-fi calendar

---
+++ docs/precache-manifest.1e03c0189fe1ed7ba667df9fb58247fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "473c18ead6f9a9f3cc00e0a479eb56aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28e7aa31fcf0f83fb460",
+    "url": "/static/css/main.22b81881.chunk.css"
+  },
+  {
+    "revision": "1159eefa0628ade0b8cf",
+    "url": "/static/js/2.4707f843.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4707f843.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28e7aa31fcf0f83fb460",
+    "url": "/static/js/main.44461907.chunk.js"
+  },
+  {
+    "revision": "54ea634d4d965a226abc",
+    "url": "/static/js/runtime-main.60e93594.js"
+  }
+]);
